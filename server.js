a4cfcc87b27a384a3decfa036572c8b2e929caa8
Kartik Raja S <kartiksubu.krs@gmail.com>
2018-08-08 19:07:27 +0530

    config 1

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const path =require('path');
 const app=express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended : false}))
 
 const posts = require('./server/routes/posts');
+
 
 app.use(express.static(path.join(__dirname,'src')));
 app.use('/posts', posts);
