6c45565c471aadd4121ccaa15647828797ca14b4
Kartik Raja S <kartiksubu.krs@gmail.com>
2018-08-08 17:40:40 +0530

    With Node

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path =require('path');
+const app=express();
+
+const posts = require('./server/routes/posts');
+
+app.use(express.static(path.join(__dirname,'src')));
+app.use('/posts', posts);
+
+
+app.get('*',(req,res)=>{
+
+	res.sendFile(path.join(__dirname,'src/index.html'))
+
+});
+
+const port = process.env.PORT || 4600;
+app.listen(port,(req,res)=>{
+	console.log('Running on port' +port);
+
+});
